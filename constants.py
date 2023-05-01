54316d7e85b6e607cc9156752b25f9eec2261d4d
Ila Itayad3h <131850863+ilaItayad3h@users.noreply.github.com>
2023-05-01 20:16:49 +0330

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,26 @@
+tagPointsCount = 250 # sets number of tags
+NA = int(6.02214129e23)
+specialCharKeyString = "-<>[]/,'"
+hashingString = (
+    '+=%$@*!#:;&().|ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz?"`~'
+)
+rawKeysDict = {
+    "0": "",
+    "1": "",
+    "2": "",
+    "3": "",
+    "4": "",
+    "5": "",
+    "6": "",
+    "7": "",
+    "8": "",
+    "9": "",
+    "-": "",
+    "<": "",
+    ">": "",
+    "[": "",
+    "]": "",
+    ",": "",
+    "/": "",
+    "'": "",
+}
